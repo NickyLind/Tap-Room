3841a2141f46feda8d5570e91d223b667ce112cd
Nick Lindau <nicholaithegreat@gmail.com>
2021-04-23 10:39:24 -0700

    flush out new keg form

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+function NewKegForm(props) {
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation(
+      {
+        name: event.target.name.value,
+        type: event.target.type.value,
+        price: event.target.price.value,
+        AlcoholContent: event.target.alcoholContent.value,
+        id: v4()
+      }
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Add Keg"
+      />;
+    </React.Fragment>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm;
+
+// need reuseable form, keg, keg details 
+// add new kegform, keg, keg details, reuseable form
